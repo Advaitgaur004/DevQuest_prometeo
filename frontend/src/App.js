f89731e2a32d5dffd8034eab2618f2e3f261f80a
Ashutosh kumar <ashutoshkumar1jkl@gmail.com>
2024-01-19 17:11:07 +0530

    add new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./component/Navbar";
+import Loader from "./component/Loader";
+
+import { useState } from "react";
+import Search from "./pages/Search";
 
 function App() {
+  const [loader,setloader] = useState(true);
+  setTimeout(() => {
+    setloader(false);
+}, 5000);
   return (
-    <div className="App">
+    <div>
+{loader ? 
+<Loader/>
+:
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/aboutus" element={<About/>} />
+      <Route path="/search" element={<Search/>} />
+
      
+    </Routes>
+  </BrowserRouter>
+  }
     </div>
+    
+
+
+    
   );
 }
 
---
+++ frontend/src/component/Loader.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "../Style/Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="wrapper">
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+        <i></i>
+      </div>
+      <a href="#black" id="black"></a>
+    </div>
+  );
+};
+
+export default Loader;
